bug fix: add transformed input to the output calculation

s5/multi_ssm.py
@@ -56,22 +56,11 @@ def binary_operator(q_i, q_j):
     A_j, b_j = q_j
     return A_j * A_i, A_j * b_i + b_j
 
-def local_self_attention(input_sequence,w_q):
+def local_self_attention(x,w_q):
     """
     Implement local self attention transformation to the input sequence
     """
-    # Create the collection of matrices using advanced slicing
-    n = 3  # number of elements in each matrix
-    stride = 2  # step size between the start of each matrix
-    # Calculate the number of windows
-    num_windows = (len(input_sequence) - n) // stride + 1
-    # Use jax.vmap to create the sliding windows
-    indices = np.arange(num_windows)[:, None] * stride + np.arange(n)
-    transformed_sequence = input_sequence[indices]
-    def compute_operation(x):
-            return x.T @ w_q @ x  #FIXME - check the dimensions again
-    lsa_sequence  = jax.vmap(compute_operation)(transformed_sequence)
-    return lsa_sequence
+    return x.T @ w_q @ x
 
 def apply_ssm(Lambda_bar, B_bar, C_tilde,w_q, D, input_sequence, conj_sym, bidirectional):
     """ Compute the LxH output of discretized SSM given an LxH input.
@@ -86,8 +75,16 @@ def apply_ssm(Lambda_bar, B_bar, C_tilde,w_q, D, input_sequence, conj_sym, bidir
         Returns:
             ys (float32): the SSM outputs (S5 layer preactivations)      (L, H)
     """
-    
-    lsa_sequence = local_self_attention(input_sequence,w_q)
+    n = 3  # number of elements in each matrix
+    stride = 2  # step size between the start of each matrix
+    # Calculate the number of windows
+    num_windows = (len(input_sequence) - n) // stride + 1
+    # Use jax.vmap to create the sliding windows
+    indices = np.arange(num_windows)[:, None] * stride + np.arange(n)
+    transformed_sequence = input_sequence[indices]
+    lsa_sequence  = jax.vmap(local_self_attention,in_axes=(0,None))(transformed_sequence,w_q)    
+
+    #lsa_sequence = local_self_attention(input_sequence,w_q)
     state_init = np.zeros((input_sequence.shape[1],input_sequence.shape[1]))
 
     def f(carry, inp):
@@ -95,29 +92,11 @@ def apply_ssm(Lambda_bar, B_bar, C_tilde,w_q, D, input_sequence, conj_sym, bidir
         return carry,carry
 
     zs, _ = jax.lax.scan(f, state_init, lsa_sequence)
-    
-    os = D @ zs
-    
+    zs = C_tilde @ zs # State transformation
+    os = D @ transformed_sequence[-1] # TODO - move this transformation to train step
+    os = zs @ os    
     return os
     
-    # Lambda_elements = Lambda_bar * np.ones((input_sequence.shape[0],
-    #                                         Lambda_bar.shape[0]))
-    # Bu_elements = jax.vmap(lambda u: B_bar @ u)(input_sequence)
-    
-    # _, xs = jax.lax.associative_scan(binary_operator, (Lambda_elements, Bu_elements))
-    
-    # if bidirectional:
-    #     _, xs2 = jax.lax.associative_scan(binary_operator,
-    #                                       (Lambda_elements, Bu_elements),
-    #                                       reverse=True)
-    #     xs = np.concatenate((xs, xs2), axis=-1)
-
-    # if conj_sym:
-    #     return jax.vmap(lambda x: 2*(C_tilde @ x).real)(xs)
-    # else:
-    #     return jax.vmap(lambda x: (C_tilde @ x).real)(xs)
-
-
 class multi_S5SSM(nn.Module):
     Lambda_re_init: np.array
     #Lambda_im_init: np.array
@@ -251,7 +230,8 @@ class multi_S5SSM(nn.Module):
                 self.C_tilde = self.C
 
         # Initialize feedthrough (D) matrix
-        self.D = self.param("D", normal(stddev=1.0), (self.H,))
+        #self.D = self.param("D", normal(stddev=1.0), (self.H,))
+        self.D = self.param("D", normal(stddev=1.0), (3,))
         lambda_bar_all = []
         log_step_all = []
         # Initialize learnable discretization timescale value